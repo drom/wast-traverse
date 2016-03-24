30479b2113c3a086bf79f8e6dfad168ea9123942
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-03-23 21:01:13 -0700

    fixed #2; use VisitorKeys from wast-spec

--- index.js
+++ index.js
@@ -1,57 +1,11 @@
 'use strict';
 
-var VisitorKeys = {
-    assert_invalid: [ 'module', 'failure' ],
-    assert_return: [ 'invoke', 'expr' ],
-    assert_return_nan: [ 'invoke' ],
-    assert_trap: [ 'invoke', 'failure' ],
-    binop: [ 'left', 'right' ],
-    block: [ 'body' ],
-    br: [ 'id', 'expr' ],
-    br_if: [ 'id', 'test', 'expr' ],
-    br_table: [ 'expr', 'body' ],
-    call: [ 'id', 'expr' ],
-    call_import: [ 'id', 'expr' ],
-    call_indirect: [ 'id', 'expr' ],
-    const: [  ],
-    cvtop: [ 'expr' ],
-    else: [ 'id', 'body' ],
-    export: [ 'name', 'id' ],
-    func: [ 'id', 'param', 'result', 'body' ],
-    get_local: [ 'id' ],
-    grow_memory: [ 'expr' ],
-    identifier: [  ],
-    if: [ 'test', 'consequent', 'alternate' ],
-    import: [ 'id', 'name1', 'name2', 'params' ],
-    invoke: [ 'body' ],
-    item: [  ],
-    literal: [  ],
-    load: [ 'expr' ],
-    local: [ 'items' ],
-    loop: [ 'body', 'extra' ],
-    memory: [ 'segment' ],
-    memory_size: [  ],
-    module: [ 'body' ],
-    nop: [  ],
-    param: [ 'items' ],
-    relop: [ 'left', 'right' ],
-    result: [  ],
-    return: [ 'expr' ],
-    script: [ 'body' ],
-    segment: [ 'name' ],
-    select: [ 'test', 'consequent', 'alternate' ],
-    set_local: [ 'id', 'init' ],
-    start: [ 'id' ],
-    store: [ 'addr', 'data' ],
-    table: [ 'items' ],
-    then: [ 'id', 'body' ],
-    type: [ 'id' ],
-    unop: [ 'expr' ],
-    unreachable: [  ]
-};
+var spec = require('wast-spec');
 
 function traverse (tree, visitors) {
-    var enter, leave;
+    var enter, leave, VisitorKeys;
+
+    VisitorKeys = spec.visitorKeys;
 
     function rec (node, parent, tail) {
         var i, ilen, j, jlen, keys, key, limb, nodeKind;
